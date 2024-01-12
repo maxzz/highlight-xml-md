f9dec2ff396a27bc12679186421d6be98fc0de08
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-11 17:24:41 -0800

    WIP. combine XML and MD demos with tabs

--- src/components/2-main/index.tsx
+++ src/components/2-main/index.tsx
@@ -1,11 +1,31 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/shadcn/tabs";
 import { HighlightMdDemo } from "./2-demo/demo-highlight-md";
 import { HighlightXmlDemo } from "./2-demo/demo-highlight-xml";
 
+function TableInTabs() {
+    return (
+        <Tabs defaultValue="tab2">
+            <TabsList>
+                <TabsTrigger value="tab1">XML</TabsTrigger>
+                <TabsTrigger value="tab2">MD</TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="tab1">
+                <HighlightXmlDemo />
+            </TabsContent>
+
+            <TabsContent value="tab2">
+                <HighlightMdDemo />
+            </TabsContent>
+
+        </Tabs>
+    );
+}
+
 export function Section2_Main() {
     return (
-        <div className="h-full overflow-auto smallscroll">
-            {/* <HighlightXmlDemo /> */}
-            <HighlightMdDemo />
+        <div className="p-4 h-full overflow-auto smallscroll">
+            <TableInTabs />
         </div>
     );
 }
